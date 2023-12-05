591f4630c8437f7ae303b5eb08d2e7fcd3a5879e
Nitiya05 <nitiyarihaisy@gmail.com>
2023-12-06 03:27:23 +0700

    update login register, onboarding, bottombar, dan profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,9 @@ plugins {
 }
 
 android {
+
     namespace = "com.dicoding.pelita"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dicoding.pelita"
@@ -26,6 +27,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -33,15 +35,29 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //splashScreen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    //Page
+    implementation("com.google.android.material:material:1.10.0")
+    //navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.5")
+
 }
--- app/src/main/java/com/dicoding/pelita/MainActivity.kt
+++ app/src/main/java/com/dicoding/pelita/MainActivity.kt
@@ -2,10 +2,51 @@ package com.dicoding.pelita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.dicoding.pelita.donasi.DonasiFragment
+import com.dicoding.pelita.favorite.FavoriteFragment
+import com.dicoding.pelita.home.HomeFragment
+import com.dicoding.pelita.profil.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.favorite -> {
+                    loadFragment(FavoriteFragment())
+                    true
+                }
+                R.id.donasi -> {
+                    loadFragment(DonasiFragment())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+
+                else -> {loadFragment(HomeFragment())
+                    true}
+            }
+        }
     }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
 }
